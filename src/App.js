071618ee99d052428fe772f4936cd078a3585826
melnyczuk <howard.melnyczuk@bbc.co.uk>
2018-11-16 15:24:40 +0000

    Configure Webpack with hello world

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
+          <p>
+            helloWorld!
+          </p>
       </div>
     );
   }
