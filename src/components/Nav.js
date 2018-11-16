7a7ab3693a4ae5fc37789afff06a8bebeadc1e51
melnyczuk <howard.melnyczuk@bbc.co.uk>
2018-11-16 16:28:59 +0000

    Add test for Nav bar

---
+++ src/components/Nav.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import '../style/App.css';
+
+class Nav extends Component {
+  render(){};
+}
+
+export default Nav;
---
+++ test/components.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import App from '../src/components/App';
+import Nav from '../src/components/Nav';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
+
+describe('Nav', () => {
+  it('renders', () => {
+    const navBar = shallow(<Nav />);
+    expect(navBar);
+  });
+});
