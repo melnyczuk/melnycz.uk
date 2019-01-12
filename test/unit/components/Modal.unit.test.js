55b630786c76a653514cfafeecfb94696cc1acc3
hpm <h.melnyczuk@gmail.com>
2019-01-12 20:44:02 +0000

    Add successful test for connected Modal container

--- test/unit/components/Modal.unit.test.js
+++ test/unit/components/Modal.unit.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store' //ES6 modules
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import { Modal } from '../../../src/components/Modal';
+import ConnectedModal, { Modal } from '../../../src/components/Modal';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('Modals', () => {
+describe('Modal', () => {
   it('renders', () => {
     const modal = shallow(<Modal />);
     expect(modal.exists()).toBe(true);
@@ -26,6 +26,19 @@ describe('Modals', () => {
     const hiddenModal = shallow(<Modal classes={['hidden']} visible={false} />);
     expect(shownModal.exists('.shown')).toBe(true);
     expect(hiddenModal.exists('.hidden')).toBe(false);
+  });
+
+  describe('Modal Container', () => {
+    it('renders the connected modal container and gets props from store', () => {
+      const store = configureStore()({ visible: true, classes: ['test'], children: [] });
+      const modalContainer = shallow(
+        <Provider store={store}>
+          <ConnectedModal />
+        </Provider>
+      );
+      expect(modalContainer.exists()).toBe(true);
+      expect(mount(modalContainer.get(0)).find('.test')).toHaveLength(1);
+    });
   });
 
 });
