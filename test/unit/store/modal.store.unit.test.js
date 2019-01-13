2be07df7020a436c519f4c38216c86679a82a1c1
hpm <h.melnyczuk@gmail.com>
2019-01-13 14:52:42 +0000

    Add better linespacing

--- test/unit/store/modal.store.unit.test.js
+++ test/unit/store/modal.store.unit.test.js
@@ -1,92 +1,110 @@
 import assert from 'assert';
 import modal from '../../../src/store/modal.store';
 
-const { initialState } = modal
 const { SET_VISIBILITY } = modal.constants;
 const { setModalVisibility } = modal.actionCreators;
 const { reduceModal } = modal.reducers;
 const { selectModalVisibility } = modal.selectors;
 
-describe('Modal Action Creators', () => {
-  describe('setModalVisibility', () => {
-    it('returns false if passed false', () => {
-      const modalVisiblity = setModalVisibility({ namespace: 'test', value: false });
-      assert.strictEqual(modalVisiblity.payload.namespace, 'test');
-      assert.strictEqual(modalVisiblity.payload.value, false);
-      assert.strictEqual(modalVisiblity.type, SET_VISIBILITY);
-    });
+describe('Modal Store Functionality', () => {
 
-    it('returns true if passed true', () => {
-      const modalVisiblity = setModalVisibility({ namespace: 'test', value: true });
-      assert.strictEqual(modalVisiblity.payload.namespace, 'test');
-      assert.strictEqual(modalVisiblity.payload.value, true);
-      assert.strictEqual(modalVisiblity.type, SET_VISIBILITY);
-    });
+  describe('Modal Action Creators', () => {
 
-    it('returns nothing if passed a non-boolean value', () => {
-      const modalVisiblity = setModalVisibility({ namespace: 'test', value: 'hello' });
-      assert.strictEqual(modalVisiblity, undefined);
-    });
+    describe('setModalVisibility', () => {
 
-    it('returns nothing if passed an object with no id', () => {
-      const modalVisiblity = setModalVisibility({ value: true });
-      assert.strictEqual(modalVisiblity, undefined);
-    });
-
-    it('returns nothing if passed an empty object', () => {
-      const modalVisiblity = setModalVisibility({});
-      assert.strictEqual(modalVisiblity, undefined);
-    });
-  });
-});
-
-describe('Modal Reducers', () => {
-  it('returns the correct initial state', () => {
-    const data = reduceModal(undefined, {});
-    assert.deepStrictEqual(data, initialState);
-  });
-
-  describe('modalVisibilityReducer', () => {
-    describe('SET_VISIBILITY action', () => {
-      it('reduces the action when the action is a valid object', () => {
-        const data = reduceModal(undefined, 
-          setModalVisibility({ namespace: 'test', value: true })
-        );
-
-        assert.strictEqual(data.test.visible, true);
+      it('returns false if passed false', () => {
+        const modalVisiblity = setModalVisibility({ namespace: 'test', value: false });
+        assert.strictEqual(modalVisiblity.payload.namespace, 'test');
+        assert.strictEqual(modalVisiblity.payload.value, false);
+        assert.strictEqual(modalVisiblity.type, SET_VISIBILITY);
       });
 
-      it('returns the exisiting state when the action is not a valid object', () => {
-        const existingState = {
-          test: {
-            src: './dummySrc.jpg',
-            alt: 'dummyalt'
+      it('returns true if passed true', () => {
+        const modalVisiblity = setModalVisibility({ namespace: 'test', value: true });
+        assert.strictEqual(modalVisiblity.payload.namespace, 'test');
+        assert.strictEqual(modalVisiblity.payload.value, true);
+        assert.strictEqual(modalVisiblity.type, SET_VISIBILITY);
+      });
+
+      it('returns nothing if passed a non-boolean value', () => {
+        const modalVisiblity = setModalVisibility({ namespace: 'test', value: 'hello' });
+        assert.strictEqual(modalVisiblity, undefined);
+      });
+
+      it('returns nothing if passed an object with no id', () => {
+        const modalVisiblity = setModalVisibility({ value: true });
+        assert.strictEqual(modalVisiblity, undefined);
+      });
+
+      it('returns nothing if passed an empty object', () => {
+        const modalVisiblity = setModalVisibility({});
+        assert.strictEqual(modalVisiblity, undefined);
+      });
+
+    });
+
+  });
+
+  describe('Modal Reducers', () => {
+
+    it('returns the correct initial state', () => {
+      const data = reduceModal(undefined, {});
+      assert.deepStrictEqual(data, modal.initialState);
+    });
+
+    describe('modalVisibilityReducer', () => {
+
+      describe('SET_VISIBILITY action', () => {
+
+        it('reduces the action when the action is a valid object', () => {
+          const data = reduceModal(undefined, 
+            setModalVisibility({ namespace: 'test', value: true })
+          );
+
+          assert.strictEqual(data.test.visible, true);
+        });
+
+        it('returns the exisiting state when the action is not a valid object', () => {
+          const existingState = {
+            test: {
+              src: './dummySrc.jpg',
+              alt: 'dummyalt'
+            }
           }
-        }
 
-        const data = reduceModal(existingState, 
-          setModalVisibility({ namespace: 'test', value: 'blahhh' })
-        );
+          const data = reduceModal(existingState, 
+            setModalVisibility({ namespace: 'test', value: 'blahhh' })
+          );
 
-        assert.deepStrictEqual(data, existingState);
+          assert.deepStrictEqual(data, existingState);
+        });
+      
       });
-    });
-  });
-});
 
-describe('Modal Selectors', () => {
-  describe('selectModalVisibility', () => {
-    it('selects the correct values for the modal namespace', () => {
-      const namespace = 'testModal';
-      const state = { [namespace]: { visible: true } }
-      const data = selectModalVisibility(state, 'testModal');
-      assert.strictEqual(data, true);
     });
-    
-    it('defaults the visibility to false if there is no key for the modal', () => {
-      const state = { visible: true };
-      const data = selectModalVisibility(state);
-      assert.strictEqual(data, false);
-    });
+
   });
+
+  describe('Modal Selectors', () => {
+
+    describe('selectModalVisibility', () => {
+    
+      it('selects the correct values for the modal namespace', () => {
+        const namespace = 'testModal';
+        const state = { [namespace]: { visible: true } }
+        const data = selectModalVisibility(state, 'testModal');
+        assert.strictEqual(data, true);
+      });
+      
+      it('defaults the visibility to false if there is no key for the modal', () => {
+        const state = { visible: true };
+        const data = selectModalVisibility(state);
+        assert.strictEqual(data, false);
+    
+      });
+    
+    });
+  
+  });
+
 });
