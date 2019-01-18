7080be2dc9b4fad9c800ac790875e90277058276
melnyczuk <howard.melnyczuk@bbc.co.uk>
2019-01-18 17:31:47 +0000

    Tail Recursive selector

---
+++ src/store/selectors.js
@@ -0,0 +1,13 @@
+import { createSelector } from 'reselect';
+
+function reduceState (state, keys) {
+  if (keys.length >= 1) return (state[keys[0]]);
+  if (state && state[keys[0]]) {
+    return reduceState(state[keys[0]], [...keys.slice(1)]);
+  }
+  return null;
+}
+
+export {
+  reduceState
+};
