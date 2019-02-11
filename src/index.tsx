72b91fc171ba435c08f2ac22caf74ff1916a7dc0
melnyczuk <howard.melnyczuk@bbc.co.uk>
2019-02-11 19:55:23 +0000

    Try to add router

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import './style/index.css';
 import App from './app';
 
