1690c895aa19138ed056bbfbefc1ca905b0dec20
melnyczuk <howard.melnyczuk@bbc.co.uk>
2019-06-18 10:03:45 +0100

    Add responsive picture sizes with component

---
+++ src/components/Picture/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ImageType } from '../../types';
+import { buildSrcWithBaseUrl } from '../../utils';
+
+interface PictureProps {
+  image: ImageType;
+  baseUrl: string;
+  className: string;
+}
+
+const getSources = (image: ImageType, buildSrc: Function) =>
+  image.sizes.map((size, i) => (
+    <source
+      key={`source-${i}`}
+      srcSet={buildSrc(size)(image)}
+      media={`(max-width: ${size * 0.8}px)`}
+    />
+  )
+);
+
+const Picture = ({image, baseUrl, className}: PictureProps) => {
+  const buildSrc = buildSrcWithBaseUrl(baseUrl)('images');
+  return (
+    <picture>
+      {getSources(image, buildSrc)}
+      <img
+        className={`punctum-image ${className}`}
+        src={buildSrc(640)(image)}
+        alt={image.alt}
+      />
+    </picture>
+  );
+}
+
+export default Picture;
