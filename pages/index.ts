fee400fa44ccfb293599aadb11d69c108085a4d3
how <h.melnyczuk@gmail.com>
2019-08-08 18:48:28 +0200

    Add test pages

---
+++ pages/index.ts
@@ -0,0 +1,3 @@
+import HelloWorld from './index';
+const index = HelloWorld;
+export default index;
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HelloWorld = () => (<div>'hello world'</div>);
+
+export default HelloWorld;
