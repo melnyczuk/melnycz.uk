25577cabb2262b7ce7e243d1ef53c11ba4daee25
Howard Melnyczuk <h.melnyczuk@gmail.com>
2019-08-14 15:19:46 +0200

    Readd cv (#21)
    * Fix max bp styling
    
    * make cv html into jsx page
    
    * Add page/route to export config
    
    * Relint config file

--- next.config.js
+++ next.config.js
@@ -3,21 +3,24 @@ const { navlabels } = require('./static/db/info.json');
 const { works } = require('./static/db/works.json');
 
 module.exports = withSass({
-  exportPathMap: function () {
-    let paths = { '/': { page: '/' } };
+  exportPathMap() {
+    const paths = {
+      '/': { page: '/' },
+      cv: { page: '/cv' },
+    };
 
-    navlabels.forEach(area => {
-      paths[`/${area}`] = { page: '/[area]', query: { area } }
+    navlabels.forEach((area) => {
+      paths[`/${area}`] = { page: '/[area]', query: { area } };
     });
 
     works.filter(({ live }) => live)
       .forEach(({ namespace, area }) => {
         paths[`/${area}/${namespace}`] = {
-          page: '/[area]/[namespace]', query: { area, namespace }
-        }
+          page: '/[area]/[namespace]', query: { area, namespace },
+        };
       });
 
     return paths;
   },
-  webpack(config, options) { return config; }
+  webpack(config, options) { return config; },
 });
