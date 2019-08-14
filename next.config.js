c50159d7c5c56c20b1e4ab3d902122b1137f6dc5
how <h.melnyczuk@gmail.com>
2019-08-14 15:18:23 +0200

    Relint config file

--- next.config.js
+++ next.config.js
@@ -3,24 +3,24 @@ const { navlabels } = require('./static/db/info.json');
 const { works } = require('./static/db/works.json');
 
 module.exports = withSass({
-    exportPathMap () {
-    let paths = { 
-      '/': { page: '/' }, 
-      'cv': { page: '/cv'} 
+  exportPathMap() {
+    const paths = {
+      '/': { page: '/' },
+      cv: { page: '/cv' },
     };
 
-    navlabels.forEach(area => {
-      paths[`/${area}`] = { page: '/[area]', query: { area } }
+    navlabels.forEach((area) => {
+      paths[`/${area}`] = { page: '/[area]', query: { area } };
     });
 
     works.filter(({ live }) => live)
       .forEach(({ namespace, area }) => {
         paths[`/${area}/${namespace}`] = {
-          page: '/[area]/[namespace]', query: { area, namespace }
-        }
+          page: '/[area]/[namespace]', query: { area, namespace },
+        };
       });
 
     return paths;
   },
-    webpack(config, options) { return config; },
+  webpack(config, options) { return config; },
 });
