52479a035c48e6d980aa98040c28c8547cb9a53d
Howard Melnyczuk <h.melnyczuk@gmail.com>
2020-04-06 22:53:26 +0100

    :fire: kill test folder (#27)

--- test/jest.setup.js
+++
@@ -1,7 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
-
-const mockFetchPromise = Promise.resolve({ text: () => 'test success' });
-global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
