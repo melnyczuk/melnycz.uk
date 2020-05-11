9fe8d35cf8cf2d8a7cb91e3b8c5372d80a2803c2
melnyczuk <h.melnyczuk@gmail.com>
2020-05-11 09:50:42 +0100

    Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+import React, { FC, ComponentProps } from "react";
+import Head from "next/head";
+
+import Nav from '../Nav';
+import SiteTitle from "../SiteTitle";
+
+import {
+  meta,
+  navlabels,
+  sitetitle,
+} from '../../../static/db/info.json';
+
+import './Header.scss';
+
+interface HeaderProps {
+  page: ComponentProps<typeof Nav>['active'];
+}
+
+const Header: FC<HeaderProps> = ({ page }) => (
+  <>
+    <Head>
+      <title>{meta.title}</title>
+      <meta name="description" content={meta.description} />
+    </Head>
+    <header>
+      <SiteTitle title={sitetitle} />
+      <Nav active={page} labels={navlabels} />
+    </header>
+  </>
+);
+
+export default Header;
