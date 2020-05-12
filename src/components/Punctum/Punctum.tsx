eec4a344d3c30f18a6745a39b71fd96ed5afbe80
melnyczuk <h.melnyczuk@gmail.com>
2020-05-12 21:14:27 +0100

    fix import

--- src/components/Punctum/Punctum.tsx
+++ src/components/Punctum/Punctum.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import Link from 'next/link';
 
-import Label from '../Label';
+import Label from './Label';
 import Picture from '../Picture';
 
 import { ImageType } from '../../types';
