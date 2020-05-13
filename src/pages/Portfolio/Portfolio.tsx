f05cbd042dfb9c339502f35c2da85511381b1078
melnyczuk <h.melnyczuk@gmail.com>
2020-05-13 21:51:10 +0100

    Flatten src/page structure & consolidate Portfolio

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -1,6 +1,33 @@
 import React, { FC } from 'react';
+
+import { media as mediaDb } from '../../../static/db/media.json';
 import { works } from '../../../static/db/works.json';
-import PortfolioItem from './PortfolioItem';
+
+import { filterMediaByNamespace, imageTypeCompletionFunc } from '../../utils';
+
+import { WorkType } from '../../types';
+
+import Punctum from '../../components/Punctum';
+
+const PortfolioItem: FC<WorkType> = ({ live, namespace, title, media }) => {
+  const namespaceImages = mediaDb
+    .images
+    .map(imageTypeCompletionFunc)
+    .filter(filterMediaByNamespace(namespace));
+
+  const punctum =
+    media.punctum?.map(
+      (p) => namespaceImages.filter(({ index }) => index === p)[0]
+    )[0];
+
+  return !live ? null : (
+    <Punctum
+      href={`portfolio/${namespace}`}
+      image={punctum}
+      title={title}
+    />
+  );
+};
 
 const Portfolio: FC = () => (<>{works.map(PortfolioItem)}</>);
 
