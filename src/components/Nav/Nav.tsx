3014f6cb8ef8e80fe8a814c1a78bade2ce797eb0
melnyczuk <h.melnyczuk@gmail.com>
2020-05-13 11:19:27 +0100

    Fix nav styling

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,32 +2,27 @@ import React, { FC } from 'react';
 import Link from 'next/link';
 import './Nav.scss';
 
-export interface Props {
-  active: string;
-  labels: string[];
-}
-
 const getClassName = (active: boolean): string =>
-  active ? 'nav--label nav--label__active' : 'nav--label';
+  active ? 'nav__label nav__label--active' : 'nav__label';
 
 const capitaliseLabel = (label: string): string =>
   label.replace(/[a-z]/, (t) => t.toUpperCase());
 
-const Nav: FC<Props> = ({ labels, active }) => (
+interface NavProps {
+  active: string;
+  labels: string[];
+}
+
+const Nav: FC<NavProps> = ({ active, labels }) => (
   <nav className="nav">
-    {labels &&
-      labels.map((label) => {
-        const labelClassName = getClassName(active === label);
-        return (
-          <Link key={label} href={`/${label}`}>
-            <button type="button" className="nav--button">
-              <h4 className={labelClassName}>{capitaliseLabel(label)}</h4>
-            </button>
-          </Link>
-        );
-      })}
+    {labels?.map((label) => (
+      <Link key={label} href={`/${label}`}>
+        <button type="button" className="nav__button">
+          <h4 className={getClassName(active === label)}>{capitaliseLabel(label)}</h4>
+        </button>
+      </Link>
+    ))}
   </nav>
 );
 
 export default Nav;
-export { getClassName, capitaliseLabel };
