a9b1be4219428bdcbf0e599f0af9964c116f689e
melnyczuk <h.melnyczuk@gmail.com>
2020-05-14 21:22:47 +0100

    lint

--- src/pages/ShaderIndex/ShaderIndex.tsx
+++ src/pages/ShaderIndex/ShaderIndex.tsx
@@ -12,15 +12,15 @@ const ShaderIndex: FC = () => (
   <>
     <PageTitle>Shaders</PageTitle>
     <ul className="shader-index">
-      {Array(N).fill(0).map((_, i) => (
-        <Link key={i} href={`${SHADERS_ROUTE}/${i}`}>
-          <li className="shader-index__list-item">
-            {`${i}`}
-          </li>
-        </Link>
-      ))}
+      {Array(N)
+        .fill(0)
+        .map((_, i) => (
+          <Link key={i} href={`${SHADERS_ROUTE}/${i}`}>
+            <li className="shader-index__list-item">{`${i}`}</li>
+          </Link>
+        ))}
     </ul>
   </>
-);  
+);
 
 export default ShaderIndex;
