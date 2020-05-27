37d22701ed09929d2cf05a95c213be1dd515252e
Howard Melnyczuk <h.melnyczuk@gmail.com>
2020-05-27 19:40:23 +0100

    🍱 Automate image resizing (#41)
    * Remove resized images
    
    * Add resize script
    
    * Update root path

---
+++ static/resize.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const glob = require('glob');
+const sharp = require('sharp');
+
+const { root, sizes } = require('./info.json');
+
+glob.sync(`./${root}/**/image`).forEach((dir) => {
+  fs.readdirSync(dir)
+    .filter((f) => f.match(/[A-z]*.(jpe?g|png)/gi))
+    .forEach((img) => {
+      sizes.forEach((size) => {
+        const sizeDir = `${dir}/${size}`;
+        if (!fs.existsSync(sizeDir)) {
+          fs.mkdirSync(sizeDir);
+        }
+        const outFile = `${sizeDir}/${img.split('-')[1]}`;
+        sharp(`${dir}/${img}`).resize(size).toFile(outFile);
+      });
+    });
+});
