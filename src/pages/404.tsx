9be344850cdc7d2b4afcb37cdbab8f3f4983e255
how melnyczuk <how@melnycz.uk>
2021-01-22 12:02:48 +0000

    update boilerplate and config

---
+++ src/pages/404.tsx
@@ -0,0 +1,35 @@
+import React, { CSSProperties } from 'react';
+import { Link } from 'gatsby';
+
+import './pages.scss';
+
+const pageStyles: CSSProperties = {
+  color: '#232129',
+  padding: '96px',
+  fontFamily: '-apple-system, Roboto, sans-serif, serif',
+};
+
+const paragraphStyles: CSSProperties = {
+  marginBottom: 48,
+  fontSize: 50,
+  textAlign: 'center',
+  border: 'solid 1 black',
+};
+
+const aStyles: CSSProperties = {
+  textDecoration: 'none',
+};
+
+const NotFoundPage = ({ path }) => (
+  <main style={pageStyles}>
+    <title>Not found</title>
+    <p style={paragraphStyles}>
+      <p>{path}</p>
+      <Link style={aStyles} to="/">
+        ¯\_(ツ)_/¯
+      </Link>
+    </p>
+  </main>
+);
+
+export default NotFoundPage;
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+const pageStyles = {
+  color: '#232129',
+  padding: '96px',
+  fontFamily: '-apple-system, Roboto, sans-serif, serif',
+};
+
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      
+    </main>
+  );
+};
+
+export default IndexPage;
