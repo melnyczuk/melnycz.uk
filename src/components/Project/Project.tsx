f1c18fd09c20040e7a520b40cfb0cc98dcc2cb9a
how melnyczuk <how@melnycz.uk>
2021-05-26 18:09:30 +0200

    move hide filter logic to Project component

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -9,21 +9,24 @@ const formatTitle = (title: string): string =>
     .map((word) => word[0].toUpperCase() + word.substring(1, word.length))
     .join(' ');
 
-const Project: FC<ProjectType> = ({ name, images, texts, year }) => (
-  <div className="project">
-    <h2 className="project__title">{formatTitle(name)}</h2>
-    <h3 className="project__year">{new Date(year).getFullYear()}</h3>
-    <div className="project__text">
-      {texts.map(({ name, url }) => (
-        <Text key={name} name={name} url={url} />
-      ))}
+const Project: FC<ProjectType> = ({ hide, name, images, texts, year }) =>
+  !hide &&
+  texts &&
+  images && (
+    <div className="project">
+      <h2 className="project__title">{formatTitle(name)}</h2>
+      <h3 className="project__year">{new Date(year).getFullYear()}</h3>
+      <div className="project__text">
+        {texts.map(({ name, url }) => (
+          <Text key={name} name={name} url={url} />
+        ))}
+      </div>
+      <div className="project__images">
+        {images.map(({ name, url }) => (
+          <Image key={name} name={name} url={url} />
+        ))}
+      </div>
     </div>
-    <div className="project__images">
-      {images.map(({ name, url }) => (
-        <Image key={name} name={name} url={url} />
-      ))}
-    </div>
-  </div>
-);
+  );
 
 export default Project;
