9122e7528a04ee3d96fd355d22b764fcc79b1bad
how melnyczuk <how@melnycz.uk>
2021-05-26 18:52:17 +0200

    use scss to style 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,31 +1,13 @@
-import React, { CSSProperties } from 'react';
-import { Link } from 'gatsby';
+import React, { FC } from 'react';
+import { Link, PageProps } from 'gatsby';
+import './pages.scss';
 
-const pageStyles: CSSProperties = {
-  color: '#232129',
-  padding: '96px',
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-};
-
-const paragraphStyles: CSSProperties = {
-  marginBottom: 48,
-  fontSize: 50,
-  textAlign: 'center',
-  border: 'solid 1 black',
-};
-
-const aStyles: CSSProperties = {
-  textDecoration: 'none',
-};
-
-const NotFoundPage = ({ path }) => (
-  <main style={pageStyles}>
+const NotFoundPage: FC<PageProps> = ({ path }) => (
+  <main className="page-404">
     <title>Not found</title>
-    <p style={paragraphStyles}>
+    <p>
       <p>{path}</p>
-      <Link style={aStyles} to="/">
-        ¯\_(ツ)_/¯
-      </Link>
+      <Link to="/">¯\_(ツ)_/¯</Link>
     </p>
   </main>
 );
