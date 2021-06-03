69ca63b2bb44957b9aefc2c913521c8f7aec0150
how melnyczuk <how@melnycz.uk>
2021-06-03 15:31:17 +0200

    Use next config and burn gatsby config

---
+++ global.d.ts
@@ -0,0 +1,11 @@
+declare module '*.graphql' {
+  import { DocumentNode } from 'graphql';
+
+  const graphql: DocumentNode;
+  export default graphql;
+}
+
+declare module '*.scss' {
+  const scss: Record<string, string>;
+  export default scss;
+}
