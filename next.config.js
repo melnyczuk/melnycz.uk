ed890559468322caae41dff7594b4059f5027d1b
how melnyczuk <how@melnycz.uk>
2023-03-12 13:23:25 +0000

    Add RSS rewrite

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  rewrites: async () => [
+    {
+      source: '/rss',
+      destination: 'https://dl.dropbox.com/s/65vk4kp2aqze06s/rss.xml',
+    },
+  ],
   webpack: (config) => ({
     ...config,
     module: {
