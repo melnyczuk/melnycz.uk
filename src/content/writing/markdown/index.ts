a4ac6d93e9c338fecca7cd587476706131d679d9
how melnyczuk <how@melnycz.uk>
2023-04-11 10:04:22 +0100

    Add The City Is Not A Platform post

--- src/content/writing/markdown/index.ts
+++ src/content/writing/markdown/index.ts
@@ -1,2 +1,3 @@
 export { default as cone_of_power } from './cone_of_power.md';
 export { default as eighteen_black_cats } from './eighteen_black_cats.md';
+export { default as the_city_is_not_a_platform } from './the_city_is_not_a_platform.md';
