0ba78c96f037cc4a76068eefb563006f0c6af16b
how melnyczuk <how@melnycz.uk>
2023-05-15 19:39:09 +0100

    Add image data script

---
+++ scripts/fetchImageData.js
@@ -0,0 +1,93 @@
+const path = require('path');
+const fm = require('front-matter');
+const fs = require('fs');
+const magick = require('imagemagick');
+const https = require('follow-redirects').https;
+
+fetch = (url) =>
+  new Promise((resolve, reject) => {
+    const request = https.request(url, { method: 'GET' }, (response) => {
+      let data = [];
+      response.on('error', (error) => reject(error));
+      response.on('data', (chunk) => {
+        data.push(chunk);
+      });
+      response.on('end', () => {
+        try {
+          const result = Buffer.concat(data);
+          resolve(result);
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+    request.on('error', (e) => reject(e));
+    request.end();
+  });
+
+const root = path.join(__dirname, '../src/content/projects');
+
+const projectData = [
+  './a-tree-grew-through-my-window.md',
+  './cone-of-power.md',
+  './gail-the-bomachat.md',
+  './object-permanence.md',
+  './on-the-side-of-every-mountain-is-another-smaller-mountain.md',
+  './ratio-club.md',
+  './rrosetta.md',
+  './silver-coord.md',
+  './supercollager.md',
+  './texture-cache.md',
+  './wool-gather.md',
+];
+
+const fetchImageData = async (src) => {
+  const name = new URL(src).pathname.split('/').reverse()[0];
+  const resp = await fetch(src);
+  const { width = 0, height = 0, format = 'image/jpeg' } = await new Promise(
+    (resolve, reject) =>
+      magick.identify({ data: resp }, (err, res) => {
+        if (err) reject(err);
+        if (res) resolve(res);
+      })
+  );
+  const thumbnail = await new Promise((resolve, reject) =>
+    magick.resize(
+      {
+        srcData: resp,
+        width: Math.floor(width * 0.01),
+        height: Math.floor(height * 0.01),
+        format,
+      },
+      (err, res) => {
+        if (err) reject(err);
+        if (res) {
+          const b64 = Buffer.from(res, 'binary').toString('base64');
+          resolve(`data:image/jpeg;base64,${b64}`);
+        }
+      }
+    )
+  );
+  return { width, height, src, name, thumbnail };
+};
+
+const cacheImageData = async (slug, imgs = []) => {
+  const images = await Promise.all(
+    imgs.map(async (src) => await fetchImageData(src))
+  );
+
+  const output = images.reduce((acc, img) => ({ ...acc, [img.src]: img }), {});
+  fs.writeFileSync(
+    path.join(root, '/images/', `${slug}.json`),
+    JSON.stringify(output)
+  );
+};
+
+Object.values(projectData).forEach((src) => {
+  const abs = path.join(root, '/markdown/', src);
+  const project = fs.readFileSync(abs);
+  const { attributes } = fm(project.toString());
+  const { slug, imgs } = attributes;
+  console.log('slug', slug);
+  cacheImageData(slug, imgs).then(() => console.log('Done!', slug));
+});
---
+++ src/content/projects/images/index.ts
@@ -0,0 +1,25 @@
+import aTreeGrewThroughMyWindow from './a-tree-grew-through-my-window.json';
+import coneOfPower from './cone-of-power.json';
+import gailTheBomachat from './gail-the-bomachat.json';
+import objectPermanence from './object-permanence.json';
+import otsoemiasm from './on-the-side-of-every-mountain-is-another-smaller-mountain.json';
+import ratioClub from './ratio-club.json';
+import rrosetta from './rrosetta.json';
+import silverCoord from './silver-coord.json';
+import supercollager from './supercollager.json';
+import textureCache from './texture-cache.json';
+import woolGather from './wool-gather.json';
+
+export {
+  aTreeGrewThroughMyWindow,
+  coneOfPower,
+  gailTheBomachat,
+  objectPermanence,
+  otsoemiasm,
+  ratioClub,
+  rrosetta,
+  silverCoord,
+  supercollager,
+  textureCache,
+  woolGather,
+};
