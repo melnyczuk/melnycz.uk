79f84d44414f96bb674209b725254901c44efa75
how melnyczuk <how@melnycz.uk>
2023-07-06 12:01:55 +0100

    Simplify function

--- src/content/parse.ts
+++ src/content/parse.ts
@@ -3,7 +3,6 @@ import fm from 'front-matter';
 import { MarkdownType } from '../types';
 
 export function parse<T extends MarkdownType>(markdown: string): T {
-  const parsed = fm<T>(markdown);
-  const { attributes, body } = parsed;
+  const { attributes, body } = fm<T>(markdown);
   return { ...attributes, body };
 }
