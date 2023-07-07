43aa5304bb1d1f1da777ea65afb227c99beeb9db
how melnyczuk <how@melnycz.uk>
2023-07-07 12:10:14 +0100

    Expose file

--- src/content/writing/markdown/index.ts
+++ src/content/writing/markdown/index.ts
@@ -1,3 +1,4 @@
 export { default as cone_of_power } from './cone_of_power.md';
 export { default as eighteen_black_cats } from './eighteen_black_cats.md';
 export { default as the_city_is_not_a_platform } from './the_city_is_not_a_platform.md';
+export { default as the_internet_as_a_libidinal_space } from './the_internet_as_a_libidinal_space.md';
