7e18c787607a2c51690ded96f14a9a88f54e2619
How <how@melnycz.uk>
2023-12-29 16:58:04 +0000

    Add Anomic Garden post

--- src/content/feed/markdown/index.ts
+++ src/content/feed/markdown/index.ts
@@ -1,3 +1,4 @@
 export { default as _20230710 } from './20230710.md';
 export { default as _20230724 } from './20230724.md';
 export { default as _20231112 } from './20231112.md';
+export { default as _20231229 } from './20231229.md';
